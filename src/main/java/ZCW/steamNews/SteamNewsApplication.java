e1d7a09bd210a219ecfde481a3e921dbee3218c4
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-04-10 19:23:51 -0400

    initial commit

---
+++ src/main/java/ZCW/steamNews/SteamNewsApplication.java
@@ -0,0 +1,13 @@
+package ZCW.steamNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ZCW/steamNews/SteamNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package ZCW.steamNews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
